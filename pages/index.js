8a201b4a010520a8e07608bd1da50dc00059f8ea
msweeneydev <mail@msweeneydev.com>
2019-06-21 00:59:15 +0100

    final before icon

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import Butter from 'buttercms';
-import ReactHtmlParser from 'react-html-parser';
-
-const butter = Butter(process.env.API_TOKEN);
+import Post from '../components/post';
 
 function HomePage() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     async function getPosts() {
-      const { data } = await butter.post.list({ page: 1, page_size: 10 });
-      const allPosts = data.data;
+      const res = await fetch(
+        `https://api.buttercms.com/v2/content/?keys=posts&auth_token=${
+          process.env.API_TOKEN
+        }`
+      );
+      const { data } = await res.json();
+      const allPosts = data.posts;
+      console.log(allPosts);
       setPosts([...allPosts]);
     }
     getPosts();
@@ -25,11 +28,46 @@ function HomePage() {
           type="text/css"
         />
       </Head>
-      {posts.length > 0 ? (
-        posts.map(p => ReactHtmlParser(p.body))
-      ) : (
-        <h1>Loading ButterCMS Content...</h1>
-      )}
+      <header>
+        <div>
+          <img src="/icons/next.svg" />
+          <img src="/icons/contentful.svg" />
+        </div>
+        <h1>Next.js + ButterCMS</h1>
+      </header>
+      <hr />
+      {posts.length > 0
+        ? posts.map(p => (
+            <Post
+              alt={p.alt}
+              date={p.date}
+              key={p.title}
+              image={p.image}
+              title={p.title}
+              url={p.url}
+            />
+          ))
+        : null}
+      <style jsx>{`
+        header {
+          height: 9.5em;
+          margin-top: 3em;
+        }
+        img {
+          margin-right: 8px;
+        }
+        hr {
+          margin 48px 0;
+        }
+        @media screen and (max-width: 360px) {
+          header {
+            height: 6em;
+          }
+          h1 {
+            font-size: 28px;
+          }
+        }
+      `}</style>
     </>
   );
 }
