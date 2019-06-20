4d01c6d0ac83bf21523f7492c284bc976e2e3740
msweeneydev <mail@msweeneydev.com>
2019-06-20 23:17:48 +0100

    add buttercms

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,45 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import Post from '../components/post';
+import Butter from 'buttercms';
 
-const client = require('contentful').createClient({
-  space: process.env.SPACE_ID,
-  accessToken: process.env.ACCESS_TOKEN
-});
+const butter = Butter(process.env.API_TOKEN);
 
 function HomePage() {
-  async function fetchContentTypes() {
-    const types = await client.getContentTypes();
-    if (types.items) return types.items;
-    console.log('Error getting Content Types.');
-  }
-  async function fetchEntriesForContentType(contentType) {
-    const entries = await client.getEntries({
-      content_type: contentType.sys.id
-    });
-    if (entries.items) return entries.items;
-    console.log(`Error getting Entries for ${contentType.name}.`);
-  }
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    async function getPosts() {
-      const contentTypes = await fetchContentTypes();
-      const allPosts = await fetchEntriesForContentType(contentTypes[0]);
-      setPosts([...allPosts]);
-    }
-    getPosts();
-  }, []);
-  console.log(posts);
+  butter.post.list({ page: 1, page_size: 10 }).then(function(response) {
+    console.log(response);
+  });
+  // async function fetchContentTypes() {
+  //   const types = await client.getContentTypes();
+  //   if (types.items) return types.items;
+  //   console.log('Error getting Content Types.');
+  // }
+  // async function fetchEntriesForContentType(contentType) {
+  //   const entries = await client.getEntries({
+  //     content_type: contentType.sys.id
+  //   });
+  //   if (entries.items) return entries.items;
+  //   console.log(`Error getting Entries for ${contentType.name}.`);
+  // }
+  // const [posts, setPosts] = useState([]);
+  // useEffect(() => {
+  //   async function getPosts() {
+  //     const contentTypes = await fetchContentTypes();
+  //     const allPosts = await fetchEntriesForContentType(contentTypes[0]);
+  //     setPosts([...allPosts]);
+  //   }
+  //   getPosts();
+  // }, []);
   return (
     <>
       <Head>
-        <title>Next.js + Contentful</title>
+        <title>Next.js + ButterCMS</title>
         <link
           rel="stylesheet"
           href="https://css.zeit.sh/v1.css"
           type="text/css"
         />
       </Head>
-      <header>
-        <div>
-          <img src="/icons/next.svg" />
-          <img src="/icons/contentful.svg" />
-        </div>
-        <h1>Next.js + Contentful</h1>
-      </header>
-      <hr />
-      {posts.length > 0
-        ? posts.map(p => (
-            <Post
-              alt={p.fields.alt}
-              date={p.fields.date}
-              key={p.fields.title}
-              image={p.fields.image}
-              title={p.fields.title}
-              url={p.fields.url}
-            />
-          ))
-        : null}
+      <h1>hi</h1>
       <style jsx>{`
         header {
           height: 9.5em;
